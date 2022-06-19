93798d834fc10c296055f4b4663496001270de3c
Steve KOUNA <stevekouna@gmail.com>
2022-06-19 18:23:30 +0400

    init commit

---
+++ src/main/java/biz/koona/gov_poll/GovPollApplication.java
@@ -0,0 +1,13 @@
+package biz.koona.gov_poll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/koona/gov_poll/GovPollApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.koona.gov_poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
